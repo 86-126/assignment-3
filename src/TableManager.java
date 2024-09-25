96a583bb02f6901b36cbc42df0dfd7f9353be94b
User <noobmultithread@gmail.com>
2024-09-25 19:52:03 +0600

    moveToSixthTable function implemented

--- src/TableManager.java
+++ src/TableManager.java
@@ -17,4 +17,18 @@ public class TableManager {
         // Simulate deadlock detection logic (for simplicity, return true randomly)
         return random.nextBoolean();  // Randomly trigger deadlock for now
     }
+
+    // Move philosopher to the 6th table if deadlocked
+    public synchronized void moveToSixthTable(int philosopherId) {
+        for (int i = 0; i < 6; i++) {
+            if (tables[i].contains(philosopherId)) {
+                tables[i].remove(Integer.valueOf(philosopherId));
+                tables[5].add(philosopherId);  // Move to the 6th table
+                System.out.println("Philosopher " + philosopherId + " moved to sixth table.");
+                break;
+            }
+        }
+    }
+
+
 }
